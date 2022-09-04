c3e9881f7106066b19e709fb81401a166c5f7644
Kiril-Pavlov <Kire_P@yahoo.com>
2022-09-04 19:42:47 +0200

    rendered the tasks and added CSS

--- src/components/ToDoApp.jsx
+++ src/components/ToDoApp.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import { useState } from 'react'
+import { AiFillCheckCircle, AiFillEdit ,AiFillDelete } from "react-icons/ai";
 
 import "./ToDoApp.css"
 
 const ToDoApp = () => {
 
-  const [taskInput,setTaskInput] = useState("");
+  const [taskInput, setTaskInput] = useState("");
   const [taskList, setTaskList] = useState([]);
 
-  const addTask = (e)=> {
-    if(taskInput){
-      let newTask ={
-        id : new Date().getTime().toString(),
-        title : taskInput
+  const addTask = (e) => {
+    if (taskInput) {
+      let newTask = {
+        id: new Date().getTime().toString(),
+        title: taskInput
       }
-      setTaskList(...taskList, newTask);
+      console.log(taskList)
+      setTaskList([...taskList, newTask]);
+      localStorage.setItem("localTaskList", JSON.stringify([...taskList, newTask]))
+      setTaskInput("")
+      console.log(taskList)
     }
   }
 
@@ -24,26 +29,46 @@ const ToDoApp = () => {
         Kiril's Todo App
       </h1>
       <div className='add-task-container'>
-        <input 
-          type="text" 
-          name='text' 
-          value="" 
+        <input
+          type="text"
+          name='text'
+          value={taskInput}
           placeholder='Enter task here...'
           className='task-input'
-          onChange={(e)=>setTaskInput(e.target.value)} 
+          onChange={(e) => setTaskInput(e.target.value)}
         />
         <button className='add-task-button' onClick={addTask}>
           Add Task
         </button>
       </div>
-      <span className='task-counter'>
-        You have 
+      <div className='task-counter'>
+        You have
         {
           !taskList.length ? " no tasks"
-          : taskList.length === 1 ? " 1 task"
-            :taskList.length>1 ? ` ${taskList.length} tasks`:null
+            : taskList.length === 1 ? " 1 task"
+              : taskList.length > 1 ? ` ${taskList.length} tasks` : null
         }
-      </span>
+      </div>
+      <div className='task-list-container'>
+        {taskList.map((task) => {
+          return (
+            <div className='task-item' key={task.id}>
+              <div className='task-title'>
+                {task.title}
+              </div>
+              <div className='complete-button'>
+                <AiFillCheckCircle />
+              </div>
+              <div className='edit-button'>
+                <AiFillEdit />
+              </div>
+              <div className='delete-button'>
+                <AiFillDelete />
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
