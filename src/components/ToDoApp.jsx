f6bae4dacfa4624df59b86b1255be922c7d1b9c0
Kiril-Pavlov <Kire_P@yahoo.com>
2022-09-04 17:53:47 +0200

    added background and some CSS to the main container

---
+++ src/components/ToDoApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import "./ToDoApp.css"
+
+const ToDoApp = () => {
+  return (
+    <div className='main-container'>
+      Teasr
+    </div>
+  )
+}
+
+export default ToDoApp
